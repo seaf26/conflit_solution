45ff7e2df2ad000c5ecbc73f9a9119b0acec5052
Seaf mohamed Gamel Abdullah <seaf222101230@ksiu.edu.eg>
2024-07-18 21:32:44 +0300

    feat: Add language switcher functionality and styles

--- src/Components/LanguageSwitcher/LanguageSwitcher.js
+++ src/Components/LanguageSwitcher/LanguageSwitcher.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './LanguageSwitcher.module.css';
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
 
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem('language') || 'ar';
+    i18n.changeLanguage(savedLanguage);
+    document.body.dir = savedLanguage === 'ar' ? 'rtl' : 'ltr';
+
+  }, [i18n]);
+
   const changeLanguage = (language) => {
     i18n.changeLanguage(language);
+    localStorage.setItem('language', language); 
+    document.body.dir = language === 'en' ? 'ltr' : 'rtl';
   };
 
   return (
-    <select id={styles.translate} onChange={(e) => changeLanguage(e.target.value)}>
+    <select
+      id={styles.translate}
+      onChange={(e) => changeLanguage(e.target.value)}
+      value={i18n.language}
+    >
       <option value="ar">العربية</option>
       <option value="en">English</option>
     </select>
