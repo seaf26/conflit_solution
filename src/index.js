d929dd9c482ccc24f8079fe1ab9de9a73b94bead
Seaf mohamed Gamel Abdullah <seaf222101230@ksiu.edu.eg>
2024-08-20 00:57:49 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './Components/i18n/i18n'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+import App from './App';
+import './index.css';
+import './Components/i18n/i18n';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-
